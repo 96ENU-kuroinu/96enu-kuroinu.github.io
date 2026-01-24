abd46f910ca347928fe54ff4cb6b49af375aaf7c
96ENU <96enu.illustration@gmail.com>
2026-01-24 19:39:39 +0900

    BaconJamマニュアル追加中…

--- js/load-version.js
+++ js/load-version.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const versionElements = document.querySelectorAll('#version');
+    const versionElements = document.querySelectorAll('#version-tidy-drive');
     if (versionElements) {
         versionElements.forEach((versionElement) => {
             versionElement.textContent = "Ver 1.8.1";
+        })
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const versionElements = document.querySelectorAll('#version-baconjam');
+    if (versionElements) {
+        versionElements.forEach((versionElement) => {
+            versionElement.textContent = "Ver 1.11.2";
         })
     }
 });
