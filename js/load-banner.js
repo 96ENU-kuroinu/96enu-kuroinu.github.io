5e48fb37102cc6f22299db33a130fa7b6c6f01ce
96enu-mac <96enu.illustration@gmail.com>
2026-02-04 02:21:26 +0900

    バナー部品修正

--- js/load-banner.js
+++ js/load-banner.js
@@ -1,16 +1,17 @@
 (function () {
-  const MOBILE_MAX_WIDTH = 767;
+    const MOBILE_MAX_WIDTH = 767;
 
-  const isMobile = window.matchMedia(
-    `(max-width: ${MOBILE_MAX_WIDTH}px)`
-  ).matches;
+    const isMobile = window.matchMedia(
+        `(max-width: ${MOBILE_MAX_WIDTH}px)`
+    ).matches;
 
-  const script = document.createElement("script");
-  script.src = isMobile
-    ? "https://adm.shinobi.jp/s/86618523177f2c4a1819c7d5733ac04c"
-    : "https://adm.shinobi.jp/s/d803ce14cb8127f899862ee566509aa8";
+    const script = document.createElement("script");
+    script.src = isMobile
+        ? "https://adm.shinobi.jp/s/86618523177f2c4a1819c7d5733ac04c"
+        : "https://adm.shinobi.jp/s/d803ce14cb8127f899862ee566509aa8";
 
-  script.defer = true; // DOM構築後に実行させたい場合
+    script.async = true;
 
-  document.head.appendChild(script);
+
+    document.head.appendChild(script);
 })();
