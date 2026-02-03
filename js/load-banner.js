751c459322bb08fa40d91b72859e073faf8a7c5d
96enu-mac <96enu.illustration@gmail.com>
2026-02-04 02:13:39 +0900

    バナー広告をレスポンシブ表示に変更中

---
+++ js/load-banner.js
@@ -0,0 +1,16 @@
+(function () {
+  const MOBILE_MAX_WIDTH = 767;
+
+  const isMobile = window.matchMedia(
+    `(max-width: ${MOBILE_MAX_WIDTH}px)`
+  ).matches;
+
+  const script = document.createElement("script");
+  script.src = isMobile
+    ? "https://adm.shinobi.jp/s/86618523177f2c4a1819c7d5733ac04c"
+    : "https://adm.shinobi.jp/s/d803ce14cb8127f899862ee566509aa8";
+
+  script.defer = true; // DOM構築後に実行させたい場合
+
+  document.head.appendChild(script);
+})();
