3d96341526bd5d1b6e2475747f4ca46da8b84fe8
96enu-mac <96enu.illustration@gmail.com>
2026-04-14 22:45:27 +0900

    0.1.4仕様に更新

--- js/load-version.js
+++ js/load-version.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const versionElements = document.querySelectorAll('#version-mispla');
     if (versionElements) {
         versionElements.forEach((versionElement) => {
-            versionElement.textContent = "Ver 0.1.3";
+            versionElement.textContent = "Ver 0.1.4";
         })
     }
 });
