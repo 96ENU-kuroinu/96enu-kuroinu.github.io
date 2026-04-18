9ef517d4388f1f51c112ecac672809fd704f6458
96enu-mac <96enu.illustration@gmail.com>
2026-04-18 15:07:23 +0900

    MisPla0.1.7仕様に更新

--- js/load-version.js
+++ js/load-version.js
@@ -1,26 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const versionElements = document.querySelectorAll('#version-tidy-drive');
-    if (versionElements) {
-        versionElements.forEach((versionElement) => {
-            versionElement.textContent = "Ver 1.8.8";
-        })
-    }
+  const versionElements = document.querySelectorAll('#version-tidy-drive');
+  if (versionElements) {
+    versionElements.forEach((versionElement) => {
+      versionElement.textContent = "Ver 1.8.8";
+    })
+  }
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-    const versionElements = document.querySelectorAll('#version-baconjam');
-    if (versionElements) {
-        versionElements.forEach((versionElement) => {
-            versionElement.textContent = "Ver 1.11.2";
-        })
-    }
+  const versionElements = document.querySelectorAll('#version-baconjam');
+  if (versionElements) {
+    versionElements.forEach((versionElement) => {
+      versionElement.textContent = "Ver 1.11.2";
+    })
+  }
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-    const versionElements = document.querySelectorAll('#version-mispla');
-    if (versionElements) {
-        versionElements.forEach((versionElement) => {
-            versionElement.textContent = "Ver 0.1.4";
-        })
-    }
+  const versionElements = document.querySelectorAll('#version-mispla');
+  if (versionElements) {
+    versionElements.forEach((versionElement) => {
+      versionElement.textContent = "Ver 0.1.7";
+    })
+  }
 });
