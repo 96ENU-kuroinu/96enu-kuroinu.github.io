883efdc43566fd0541ea99b4bcb7d65e542cecd8
96enu-mac <96enu.illustration@gmail.com>
2026-05-03 01:25:00 +0900

    MisPla 1.0.4仕様に更新

--- js/load-version.js
+++ js/load-version.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const versionElements = document.querySelectorAll('#version-mispla');
   if (versionElements) {
     versionElements.forEach((versionElement) => {
-      versionElement.textContent = "Ver 1.0.1";
+      versionElement.textContent = "Ver 1.0.4";
     })
   }
 });
